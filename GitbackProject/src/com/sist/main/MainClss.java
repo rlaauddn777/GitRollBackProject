3fff6d3b76f22864af05bdc922192ca24b28427c
user <user@DESKTOP-COJ9858>
2022-07-04 18:32:23 +0900

    back2

--- GitbackProject/src/com/sist/main/MainClss.java
+++ GitbackProject/src/com/sist/main/MainClss.java
@@ -5,6 +5,7 @@ public class MainClss {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hdelldod git");
+		System.out.println("hdelldod git2");
 	}
 
 }
